f0cae8e8e152f35dae724d7c3a00bb7daab703ba
MaraMarcelino <marammarcelino@gmail.com>
2023-05-31 17:02:40 -0300

    deploy

--- app.js
+++ app.js
@@ -1,218 +1,35 @@
-/**
- * Copyright 2021-present, Facebook, Inc. All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * Messenger For Original Coast Clothing
- * https://developers.facebook.com/docs/messenger-platform/getting-started/sample-apps/original-coast-clothing
- */
-
-"use strict";
-
-// Import dependencies and set up http server
-const express = require("express"),
-  { urlencoded, json } = require("body-parser"),
-  crypto = require("crypto"),
-  path = require("path"),
-  Receive = require("./services/receive"),
-  GraphApi = require("./services/graph-api"),
-  User = require("./services/user"),
-  config = require("./services/config"),
-  i18n = require("./i18n.config"),
-  app = express();
-
-var users = {};
-
-// Parse application/x-www-form-urlencoded
-app.use(
-  urlencoded({
-    extended: true
-  })
-);
-
+const express = require('express');
+const app = express();
 const json_encode = require('json_encode');
 
-// Parse application/json. Verify that callback came from Facebook
-app.use(json({ verify: verifyRequestSignature }));
-
-// Serving static files in Express
-app.use(express.static(path.join(path.resolve(), "public")));
-
-// Set template engine in Express
-app.set("view engine", "ejs");
-
-// Respond with index file when a GET request is made to the homepage
+app.use(express.json());
+ 
 app.get("/", function (_req, res) {
   res.render("index");
 });
 
-// Add support for GET requests to our webhook
+
 app.get("/webhook", (req, res) => {
   // Parse the query params
   let mode = req.query["hub.mode"];
   let token = req.query["hub.verify_token"];
   let challenge = req.query["hub.challenge"];
 
-  // Check if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Check the mode and token sent is correct
-    if (mode === "subscribe" && token === config.verifyToken) {
-      // Respond with the challenge token from the request
-      console.log("WEBHOOK_VERIFIED");
+  console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
-    } else {
-      // Respond with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
-  }
 });
 
-// Create the endpoint for your webhook
 app.post("/webhook", (req, res) => {
   let body = req.body;
+  var encoded = json_encode(body);
 
-  var encoded = json_encode(req.body);
- 
-  console.log(encoded);
   return res.json({
     erro: false,
     mensagem: "Sucesso!"
 });
+
 });
 
-function setDefaultUser(id) {
-  let user = new User(id);
-  users[id] = user;
-  i18n.setLocale("en_US");
-}
-
-function isGuestUser(webhookEvent) {
-  let guestUser = false;
-  if ("postback" in webhookEvent) {
-    if ("referral" in webhookEvent.postback) {
-      if ("is_guest_user" in webhookEvent.postback.referral) {
-        guestUser = true;
-      }
-    }
-  }
-  return guestUser;
-}
-
-function receiveAndReturn(user, webhookEvent, isUserRef) {
-  let receiveMessage = new Receive(user, webhookEvent, isUserRef);
-  return receiveMessage.handleMessage();
-}
-
-// Set up your App's Messenger Profile
-app.get("/profile", (req, res) => {
-  let token = req.query["verify_token"];
-  let mode = req.query["mode"];
-
-  if (!config.webhookUrl.startsWith("https://")) {
-    res.status(200).send("ERROR - Need a proper API_URL in the .env file");
-  }
-  var Profile = require("./services/profile.js");
-  Profile = new Profile();
-
-  // Check if a token and mode is in the query string of the request
-  if (mode && token) {
-    if (token === config.verifyToken) {
-      if (mode == "webhook" || mode == "all") {
-        Profile.setWebhook();
-        res.write(
-          `<p>&#9989; Set app ${config.appId} call to ${config.webhookUrl}</p>`
-        );
-      }
-      if (mode == "profile" || mode == "all") {
-        Profile.setThread();
-        res.write(
-          `<p>&#9989; Set Messenger Profile of Page ${config.pageId}</p>`
-        );
-      }
-      if (mode == "personas" || mode == "all") {
-        Profile.setPersonas();
-        res.write(`<p>&#9989; Set Personas for ${config.appId}</p>`);
-        res.write(
-          "<p>Note: To persist the personas, add the following variables \
-          to your environment variables:</p>"
-        );
-        res.write("<ul>");
-        res.write(`<li>PERSONA_BILLING = ${config.personaBilling.id}</li>`);
-        res.write(`<li>PERSONA_CARE = ${config.personaCare.id}</li>`);
-        res.write(`<li>PERSONA_ORDER = ${config.personaOrder.id}</li>`);
-        res.write(`<li>PERSONA_SALES = ${config.personaSales.id}</li>`);
-        res.write("</ul>");
-      }
-      if (mode == "nlp" || mode == "all") {
-        GraphApi.callNLPConfigsAPI();
-        res.write(
-          `<p>&#9989; Enabled Built-in NLP for Page ${config.pageId}</p>`
-        );
-      }
-      if (mode == "domains" || mode == "all") {
-        Profile.setWhitelistedDomains();
-        res.write(
-          `<p>&#9989; Whitelisted domains: ${config.whitelistedDomains}</p>`
-        );
-      }
-      if (mode == "private-reply") {
-        Profile.setPageFeedWebhook();
-        res.write(`<p>&#9989; Set Page Feed Webhook for Private Replies.</p>`);
-      }
-      res.status(200).end();
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
-  } else {
-    // Returns a '404 Not Found' if mode or token are missing
-    res.sendStatus(404);
-  }
-});
-
-// Verify that the callback came from Facebook.
-function verifyRequestSignature(req, res, buf) {
-  var signature = req.headers["x-hub-signature"];
-
-  if (!signature) {
-    console.warn(`Couldn't find "x-hub-signature" in headers.`);
-  } else {
-    var elements = signature.split("=");
-    var signatureHash = elements[1];
-    var expectedHash = crypto
-      .createHmac("sha1", config.appSecret)
-      .update(buf)
-      .digest("hex");
-    if (signatureHash != expectedHash) {
-      throw new Error("Couldn't validate the request signature.");
-    }
-  }
-}
-
-// Check if all environment variables are set
-config.checkEnvVariables();
-
-// Listen for requests :)
-var listener = app.listen(config.port, function () {
-  console.log(`The app is listening on port ${listener.address().port}`);
-  if (
-    Object.keys(config.personas).length == 0 &&
-    config.appUrl &&
-    config.verifyToken
-  ) {
-    console.log(
-      "Is this the first time running?\n" +
-        "Make sure to set the both the Messenger profile, persona " +
-        "and webhook by visiting:\n" +
-        config.appUrl +
-        "/profile?mode=all&verify_token=" +
-        config.verifyToken
-    );
-  }
-
-  if (config.pageId) {
-    console.log("Test your app by messaging:");
-    console.log(`https://m.me/${config.pageId}`);
-  }
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor iniciado");
 });
